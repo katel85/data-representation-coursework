1f3fc9ce5cc2852fed8512664376a5f751780388
Catherine Leddy <G00048625@gmit.ie>
2023-01-15 23:43:19 +0000

    removed incorrectfiles

--- Assignments/Assignment04-github.py
+++ Assignments/Assignment04-github.py
@@ -7,12 +7,6 @@ from config import config as cfg
 apikey = cfg["githubkey"]
 g = Github(apikey)
 
-#for repo in g.get_user().get_repos():
-    #print(repo.name)
-    #repo.edit(has_wiki=False)
-    # to see all the available attributes and methods
-    #print(dir(repo))
-# make sure this replository exists, and that the path is correct
 # https://github.com/katel85/aprivateone.git
 privaterepo="katel85/aprivateone"
 repo = g.get_repo(privaterepo)
@@ -22,11 +16,13 @@ fileInfo = repo.get_contents("test.txt")
 urlOfFile= fileInfo.download_url
 #print (urlOfFile)
 response = requests.get(urlOfFile)
+# output the contents of the file
 contentOfFile = response.text
 #print (contentOfFile)
 #change contents
 newContent= contentOfFile.replace("Andrew","Kate")
 #print (newContents)
+# sha(unique ID for the commit) as parameters
 gitHubResponse=repo.update_file(fileInfo.path,"udated names",newContent,fileInfo.sha)
 print (gitHubResponse)
 
