154b514a7d6e57328d80010ee9ecec0d76191992
katel85 <77672629+katel85@users.noreply.github.com>
2023-01-17 09:48:14 +0000

    Update config.py

--- Labs/Week5 API-OAuth/config.py
+++ Labs/Week5 API-OAuth/config.py
@@ -1,6 +1,6 @@
 # API CREATED FROM https://dash.html2pdf.app/
 
 config = {
-    "htmltopdfkey": "5kmrWnpUTjpHeRSQwbyoY5dbcgDN60IJybLteWAo7bkAV0kBd7EEITsB1Mn7EAhR",
-    "githubkey" : "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"
+    "htmltopdfkey": "",
+    "githubkey" : ""
 }
